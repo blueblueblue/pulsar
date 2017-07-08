2c03f60c8e70915fed41a8bf3e43e28ebfa6d2ba
Luc Perkins <lucperkins@gmail.com>
2017-07-08 15:25:15 -0700

    modify build script

--- site/scripts/build-all-versions.sh
+++ site/scripts/build-all-versions.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-BUNDLER_VERSION=$(cat package.json | jq .versions.bundler | tr -d '"')
+BUNDLER_VERSION=1.15.1
 
 for version in $(cat VERSIONS); do
   bundle _${BUNDLER_VERSION}_ exec jekyll build \
     --config _config.yml,docs/$version/_config.version.yml
 done
-
-git checkout asf-site
-
-(
-  cd $(git rev-parse --show-toplevel)
-  rm -rf api css fonts img index.html ja js
-  mv generated-site/* .
-  git add -A
-  git commit -m "New site build $(date)"
-  git checkout master
-  rm -rf api css fonts img index.html ja js
-)
